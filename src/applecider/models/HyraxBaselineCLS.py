f32ee7dc760ce235512c83ac1b50a1b5c1e637c8
drewoldag <47493171+drewoldag@users.noreply.github.com>
2025-11-06 10:59:52 -0800

    Renaming example notebooks, and updating some imports.

--- src/applecider/models/HyraxBaselineCLS.py
+++ src/applecider/models/HyraxBaselineCLS.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from ...._archive.AppleCider.models.BaselineCLS import Time2Vec
+from applecider.models.Time2Vec import Time2Vec
 
 
 @hyrax_model
