6525e857128d9ead6f7e67cca21ae57c3f199814
Doug Branton <dougrbranton@gmail.com>
2025-11-21 15:05:41 -0800

    state update 11/21

--- src/applecider/datasets/photo_dataset.py
+++ src/applecider/datasets/photo_dataset.py
@@ -4,19 +4,22 @@ from pathlib import Path
 from typing import Union
 import numpy as np
 from torch.nn.utils.rnn import pad_sequence
+from applecider.datasets.oversampler_mixin import OversamplerMixin
 import torch
 import pandas as pd
 
 
-class PhotoEventsDataset(HyraxDataset, Dataset):
+class PhotoEventsDataset(HyraxDataset, Dataset, OversamplerMixin):
     def __init__(self, config: dict, data_location: Union[Path, str] = None, horizon: float = 10.0):
         self.data_location = data_location
         self.filenames = sorted(list(Path(self.data_location).glob('*.npz')))
         #self.manifest_df = pd.read_csv(config["data_set"]["PhotoEventsDataset"]["manifest_path"])
         self.manifest_df = pd.read_csv(config["data_set"]["applecider.datasets.photo_dataset.PhotoEventsDataset"]["manifest_path"])
+        self.manifest_df = self.manifest_df.sort_values("obj_id", inplace=False)
         self.object_ids = self.manifest_df["obj_id"].tolist()
         self.horizon = config["data_set"]["PhotoEventsDataset"]["horizon"]
         self.st = np.load(Path(config["data_set"]["PhotoEventsDataset"]["stats_path"]))
+        self.use_oversampling = config["data_set"]["PhotoEventsDataset"]["use_oversampling"]
 
         # Map original subclass IDs to broader classes
         self.taxonomy_mapper = {0: 0,  # SN Ia -> SNI
@@ -31,16 +34,23 @@ class PhotoEventsDataset(HyraxDataset, Dataset):
                                 9: 4,  # Tidal Disruption Event -> TDE
                                 }
 
-        # Do we need to define test vs train data?
+        ideal_class_distribution = config["data_set"]["PhotoEventsDataset"]["ideal_class_distribution"]
+        class_at_index = [self.taxonomy_mapper[label] for label in
+                          self.manifest_df.label.tolist()]
+        if self.use_oversampling:
+            self.prepare_over_sampling(ideal_class_distribution, class_at_index)
         super().__init__(config)
 
     def __getitem__(self, idx):
         # getting happens via the getter methods below
         pass
-    
+
     def get_object_id(self, idx):
         """get unique identifier for a specific index"""
         # Find the row in the manifest ids
+        old_idx = idx
+        if self.use_oversampling:
+            idx, is_oversampled = self.retrieve_oversampled_index(idx)
         return self.object_ids[idx]
 
     def ids(self):
@@ -49,21 +59,26 @@ class PhotoEventsDataset(HyraxDataset, Dataset):
 
     def get_label(self, idx):
         """get ID label for a specific index"""
+        old_idx = idx
+        if self.use_oversampling:
+            idx, is_oversampled = self.retrieve_oversampled_index(idx)
         # Find the row in the manifest
         row = self.manifest_df.iloc[idx]
-        #import pdb;pdb.set_trace()
         return self.taxonomy_mapper[row.label]
         #return self.id2broad_id[int(row.label)]
 
     def get_photometry(self, idx):
         """get photometry tensor for a specific index"""
+        #if config["use_oversampling"]
+        if self.use_oversampling:
+            idx, is_oversampled = self.retrieve_oversampled_index(idx)
+        #print(self.manifest_df.iloc[idx]["obj_id"], self.filenames[idx], self.manifest_df.iloc[idx].label)
+        #import pdb; pdb.set_trace()
         data = np.load(self.filenames[idx], allow_pickle=True)["data"]
         # TODO: Consider caching data to avoid duplicate loads in each epoch
 
         # Limit length to <100 for memory constraints
         #data = data[:100]
-
-        # TODO: oversampling
 
         # TODO: data augmentation
 
@@ -72,8 +87,8 @@ class PhotoEventsDataset(HyraxDataset, Dataset):
 
 
         # Grab features from array slices
-        dt = data[:, 0]
-        dt_prev = data[:, 1]
+        dt = np.log1p(data[:, 0])
+        dt_prev = np.log1p(data[:, 1])
         logf = data[:, 3]
         logfe = data[:, 4]
         band = data[:, 2]
@@ -95,12 +110,14 @@ class PhotoEventsDataset(HyraxDataset, Dataset):
         return self.st['std']
 
     def __len__(self):
-        return len(self.filenames)
+        if self.use_oversampling:
+            return self.total_count_with_oversampling
+        else:
+            return len(self.filenames)
 
 
 def collate(batch):
     '''custom collate function for photo events dataset'''
-
     seqs = []
     labels = []
     for i in batch:
